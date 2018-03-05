cb06c45bf235a59a6b0e7974bf9a692c84962398
vagrant <vagrant@homestead>
2018-03-05 02:04:37 +0000

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -22,11 +23,18 @@ class UsersController extends Controller
      * 表单请求 UserRequest 是 Laravel 框架提供的用户表单数据验证方案
      * 此方案相比手工调用 validator 来说，能处理更为复杂的验证逻辑，更加适用于大型程序。
      */
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        dd($request->avatar);
+        $data = $request->all();
 
-        $user->update($request->all());
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
