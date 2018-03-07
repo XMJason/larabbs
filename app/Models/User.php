e6ae7f61189ea0a68ca0e1a66f1f5fdc4ecd678d
vagrant <vagrant@homestead>
2018-03-07 02:02:18 +0000

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /*
+     * 用户与话题中间的关系是一对多的关系，一个用户拥有多个主题，在Eloquent中使用 hasMany() 方法进行关联。
+     * 关联设置成功后，可使用 $user->topics 来获取到用户发布的所有话题数据。
+     */
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
