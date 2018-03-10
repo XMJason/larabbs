d5642c8b0c37f6deac084d2639d3d22a958475ed
Jason <zpj1750@163.com>
2018-03-10 12:33:37 +0000

    Author can delete topic

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -10,12 +10,14 @@ class TopicPolicy extends Policy
     public function update(User $user, Topic $topic)
     {
         // 只有当话题关联作者的ID 等于当前登录用户的ID时候才放行
-        return $topic->user_id == $user->id;
-        // return true;
+        // return $topic->user_id == $user->id;
+        return $user->isAuthorOf($topic);
     }
 
     public function destroy(User $user, Topic $topic)
     {
-        return true;
+        // 在 多个地方使用 $topic->user_id == $user->id; 代码的可读性不高。
+        // 故在 User 模型中定义 isAuthorOf 函数
+        return $user->isAuthorOf($topic);
     }
 }
