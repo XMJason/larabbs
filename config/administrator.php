52314ecf6957395d3ce65db255afb0fb98bc03b4
Jason <zpj1750@163.com>
2018-03-15 15:15:27 +0000

    Sidebar links

--- config/administrator.php
+++ config/administrator.php
@@ -45,7 +45,7 @@ return array(
         '内容管理' => [
             'categories', 'topics', 'replies',
         ],
-        '站点管理' => ['settings.site'],
+        '站点管理' => ['settings.site', 'links'],
     ],
 
     /*
