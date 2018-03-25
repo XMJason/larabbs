6147285039fee3c0cf3a261a296d73de8b019247
Jason <zpj1750@163.com>
2018-03-25 12:24:59 +0800

    rate limit

--- config/api.php
+++ config/api.php
@@ -256,4 +256,20 @@ return [
 
     ],
 
+    /*
+     * 接口频率限制
+     */
+    'rate_limits' => [
+        // 访问频率限制，次数/分钟(60/1)
+        'access' => [
+            'expires' => env('RATE_LIMITS_EXPIRES', 1),
+            'limit' => env('RATE_LIMITS', 60),
+        ],
+        // 登录相关，次数/分钟(10/1)
+        'sign' => [
+            'expires' => env('SIGN_RATE_LIMITS_EXPIRES', 1),
+            'limit' => env('SIGN_RATE_LIMITS', 10),
+        ],
+    ],
+
 ];
--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,15 @@ $api->version('v1', [
     // 增加参数，namespace，使 v1 版本的路由都会指向 App\Http\Controllers\Api
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
-    // 短信验证码接口
-    $api->post('verificationCodes', 'verificationCodesController@store')->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UsersController@store')->name('api.users.store');
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api){
+        // 短信验证码接口
+        $api->post('verificationCodes', 'verificationCodesController@store')->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')->name('api.users.store');
+    });
 });
